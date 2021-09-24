1b6856de0bd10640881d279c9729d8b9a09e44aa
Salma <salmaaly47@gmail.com>
2021-09-23 20:22:28 -0700

    updated nav bar and added javascript functions

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-$('#sponsor-carousel').carousel({
+/*$('#sponsor-carousel').carousel({
     interval: 3000,
     cycle: true
-  }); 
+  }); */
+
+
+// When button is clicked, shows dropdown content
+function showDropdown() {
+  document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+// Closes the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.classList.contains('show')) {
+      dropdownContent.classList.remove('show');
+    }
+  }
+}
