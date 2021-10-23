2752a9533cc502a8d6c4dc21fafcc9cbba1eb04e
sidiknatasha <sidiknatasha@users.noreply.github.com>
2021-10-22 23:46:36 -0700

    schedule section

---
+++ schedule.js
@@ -0,0 +1,367 @@
+const day1 = [
+	{
+		"Time": "7:00 - 7:30 PM",
+		/*"Category": "Universal",*/
+		"Title": "Hackathon 101",
+		"Host": "TT Seattle Team",
+		"Description": "",
+		
+	},
+	{
+		"Time": "7:30 - 8:00 PM",
+		/*"Category": "Activity",*/
+		"Title": "Team Formation 1",
+		"Host": "TT Seattle Team",
+		"Description": "",
+
+	},
+]
+
+const day2 = [
+	{
+		"Time": "5:00 - 5:30 PM",
+		/*"Category": "Universal",*/
+		"Title": "Opening Ceremony",
+		"Host": "TT Seattle Team",
+		"Description": "",
+		
+	},
+	{
+		"Time": "5:30 - 6:00 PM",
+		/*"Category": "Activity",*/
+		"Title": "Team Formation 2",
+		"Host": "TT Seattle Team",
+		"Description": "",
+	
+	},
+	{
+		"Time": "6:00 - 6:30 PM",
+		/*"Category": "Activity",*/
+		"Title": "Team Formation 3",
+		"Host": "TT Seattle Team",
+		"Description": "",
+		
+	},
+	{
+		"Time": "6:30 - 7:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Web Development: HTML/CSS",
+		"Host": "Gabriella Miesner",
+		"Description": "",
+		
+	},
+	{
+		
+		"Time": "7:00 - 7:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Debugging",
+		"Host": "Leslie Richardson",
+		"Description": "",
+		
+	},
+	{
+		"Time": "8:00 - 8:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to React",
+		"Host": "Rakshaa Viswanathan",
+		"Description": "",
+	
+	},
+	{
+		"Time": "8:30 - 9:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Microsoft Workshop",
+		"Host": "",
+		"Description": "",
+	
+	},
+	{
+		"Time": "9:00 - 9:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Flutter",
+		"Host": "Akanksha Singh",
+		"Description": ""
+	},
+	{
+		"Time": "10:30 - 11:30 PM",
+		/*"Category": "Activity",*/
+		"Title": "Late Night Fun",
+		"Host": "TT Seattle Team",
+		"Description": "",
+	},
+]
+
+const day3 = [
+	{
+		
+		"Time": "8:30 - 8:30 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Python",
+		"Host": "Libby Sun",
+		"Description": "",
+
+	},
+	{
+		
+		"Time": "8:30 - 9:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Brainstorming ideas for your hack",
+		"Host": "Vaishnavi Dwivedi",
+		"Description": "",
+		"Link": ""
+	},
+	{
+		
+		"Time": "9:00 - 9:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "SWE Interview Prep",
+		"Host": "Libby Sun",
+		"Description": "",
+
+	},
+	{
+		"Time": "9:30 - 10:00 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Into to SQL",
+		"Host": "Rebecca Hadi",
+		"Description": "",
+
+	},
+	{
+		"Time": "10:00 - 10:30 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Building your portfolio website",
+		"Host": "Sushree Satarupa",
+		"Description": "",
+		
+	},
+	{
+		"Time": "10:00 - 10:30 AM",
+		/*"Category": "Activity,*/
+		"Title": "Team Formation 4",
+		"Host": "TT Seattle Team",
+		"Description": "",
+
+	},
+	{
+		"Time": "10:30 - 11:00 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to iOS Dev: Xcode",
+		"Host": "Shirui Zhong",
+		"Description": "",
+		
+	},
+	{
+		"Time": "11:00 - 11:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Non-Traditional Background in Tech",
+		"Host": "Izzy Lapidus",
+		"Description": "",
+	
+	},
+	{
+		"Time": "11:30 - 12:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to APIs",
+		"Host": "Maria Sitkovets",
+		"Description": "",
+		
+	},
+	{
+		"Time": "12:00 - 12:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intersectionality with Tech & other fields",
+		"Host": "Hana Gabrielle Bidon",
+		"Description": "",
+		
+	},
+	{
+		"Time": "12:30 - 1:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Keynote Speaker Session",
+		"Host": "TBA",
+		"Description": "",
+	
+	},
+	{
+		"Time": "1:30 - 3:00 PM",
+		/*"Category": "Universal",*/
+		"Title": "Career Fair",
+		"Host": "",
+		"Description": "",
+		
+	},
+	{
+		"Time": "2:00 - 2:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Deep Learning",
+		"Host": "Kapil Yadav",
+		"Description": "",
+		
+	},
+	{
+		"Time": "3:00 - 4:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Microsoft Workshop",
+		"Host": "",
+		"Description": "",
+		
+	},
+	{
+		"Time": "4:00 - 5:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Code + Art - Create your coded portrait",
+		"Host": "Kaylla Torres",
+		"Description": "",
+		
+	},
+	{
+		"Time": "5:30 - 6:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "A Hitchhiker's Guide to Social Impact",
+		"Host": "Mingjie Jiang and Megan Cui",
+		"Description": "",
+		
+	},
+	{
+		"Time": "6:00 - 6:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to designing with Canva",
+		"Host": "Belle See",
+		"Description": "Want to quickly and easily create posters, instagram posts, presentations and everything under the umbrella of graphic design? Utilise the power of Canva to get started. We'll be covering how to use the Canva platform, utilising and modifying templates, general graphic design rules, Canva tips and tricks and wrap it all up with a mini design task.",
+		
+	},
+	{
+		"Time": "7:00 - 8:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to designing websites",
+		"Host": "Belle See",
+		"Description": "Anyone, including you, can be a website designer! Within an hour, learn about visual hierarchy, layout, design styles, and rules of website design. The second half of the workshop is a hands-on portion where you'll be able to play the role of a website designer and put theory into practice!",
+		
+	},
+	{
+		"Time": "8:00 - 8:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intersectionality with Tech & other fields",
+		"Host": "Hana Gabrielle Bidon",
+		"Description": "",
+		
+	},
+	{
+		"Time": "8:30 - 9:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Building communities  & Opportunities in Opensource",
+		"Host": "Joshua Poddoku",
+		"Description": "",
+		
+	},
+	{
+		"Time": "10:30 - 11:30 PM",
+		/*"Category": "Activity",*/
+		"Title": "Late Night Fun",
+		"Host": "TT Seattle Team",
+		"Description": "",
+	},
+]
+const day4 = [
+	{
+		
+		"Time": "9:00 - 9:30 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Take your hack to the next level",
+		"Host": "Vaishnavi Dwivedi",
+		"Description": "",
+
+	},
+	{
+		
+		"Time": "9:30 - 10:30 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Microsoft Workshop",
+		"Host": "",
+		"Description": "",
+		"Link": ""
+	},
+	{
+		
+		"Time": "11:00 - 11:30 AM",
+		/*"Category": "Universal",*/
+		"Title": "Keynote Speaker Session",
+		"Host": "Audrey Pe",
+		"Description": "",
+
+	},
+	{
+		"Time": "12:00 - 12:30 AM",
+		/*"Category": "Workshop",*/
+		"Title": "Resume-Building & LinkedIn for HS Students",
+		"Host": "Stephanie Su",
+		"Description": "",
+
+	},
+	{
+		"Time": "12:30 - 1:00 PM",
+		/*"Category": "Workshop",*/
+		"Title": "Intro to Open Source: Overcoming the Fear of Contributing to Open Source",
+		"Host": "Rizel Scarlett",
+		"Description": "I spent the last 3 years of my careers nervous to contribute to open source. The codebases seemed so large and the documentation was intimidating! Learn how I overcame that fear and how you can too!",
+		
+	},
+]
+
+document.getElementById('1').onclick = () => {fillData(day1)}
+document.getElementById('2').onclick = () => {fillData(day2)}
+document.getElementById('3').onclick = () => {fillData(day3)}
+document.getElementById('4').onclick = () => {fillData(day4)}
+
+
+document.getElementById('day').innerHTML = `
+        ${day1.map(sched_template).join('')}
+        <h5 style="font-size: 0.9rem;">Check back soon for updates.
+			<br>Please note that all times are in Pacifid Time (GMT+8).
+			<br>Click on an day to see the day's schedule.
+		</h5>
+    `
+
+function fillData(data){
+    document.getElementById('day').innerHTML = `
+        ${data.map(sched_template).join('')}
+        <h5 style="font-size: 0.9rem;">Check back soon for updates.</h5>
+    `
+}
+
+function sched_template(day_data){
+    return `
+    <div class="event">
+        <span id="category" class="circle fs-3"> ${day_data.Category.charAt(0)} </span>
+        <h4 class="fs-4 text-primary">
+            <span style="font-weight: bold;"> 
+				${day_data.Link ? `<a href=${day_data.Link}> ${day_data.Title} </a>` : `${day_data.Title}` }
+			</span>
+            <span class="fs-6" style="font-weight: bold; float:right">${day_data.Time}</span> 
+            <br>
+            <span class="fs-6" style="color: #888;"> Hosted by ${day_data.Host} </span>
+        </h4>
+        <p>${day_data.Description}</p>
+    </div>
+    `
+}
+
+{/* 
+<span id="category" class="circle">${day_data.Category.charAt(0)}</span>
+<span onclick ="${day_data.Category.charAt(0)}"></span>
+document.getElementById('box').innerHTML = `
+<span onclick ="${category_color('g')}">b</span>
+`
+${
+    document.getElementById('category').innerHTML = `
+        <span onclick ="${category_color(day_data.Category.charAt(0))}"> ${day_data.Category.charAt(0)} </span>
+    `
+}
+*/}
+
+/*function category_color(char){
+    if(char === 'U'){ document.getElementById("category").style.backgroundColor='blue' }
+    if(char === 'A'){ document.getElementById("category").style.backgroundColor='green' }
+    if(char === 'W'){ document.getElementById("category").style.backgroundColor='red' }
+}*/
--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+/*Sponsor Carousel*/
 $('#sponsor-carousel').carousel({
     interval: 3000,
     cycle: true
   });
+
+/*Back to Top*/
+var target = document.querySelector("footer");
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function callback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Show button
+      scrollToTopBtn.classList.add("showBtn");
+    } else {
+      // Hide button
+      scrollToTopBtn.classList.remove("showBtn");
+    }
+  });
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+let observer = new IntersectionObserver(callback);
+observer.observe(target);
+  
